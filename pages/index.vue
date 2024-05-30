50ab72af06e4e1913ecaa183e1230190b5227031
Asad <mmonir5072@gmail.com>
2024-05-30 23:36:17 +0600

    asad

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+    const loading = ref(false)
+</script>
+<template>
+    <div class="p-4">
+        <h1>Hello index!</h1>
+        <Button>Click me</Button>
+    </div>
+</template>
