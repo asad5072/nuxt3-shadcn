53c5eeec1110690036d3d6447d2769ef24bc5ba5
asad5072 <mmonir5072@gmail.com>
2024-06-01 11:36:45 +0600

    asdfasdf

---
+++ components/Tabs/Today.vue
@@ -0,0 +1,10 @@
+<script setup>
+  
+</script>
+<template>
+    <div>
+        <p>Hello Today</p>
+    </div>
+</template>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <script setup>
+    import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+    import TabsToday from '@/components/Tabs/Today.vue'
     const loading = ref(false)
+    const list =[
+        {
+            title: 'Today',
+            component: TabsToday
+        },
+        {
+            title: 'This Week',
+            des: 'Hello Todalyasdfwer'
+        },
+        {
+            title: 'This Month',
+            des: 'Hello Todaly2354'
+        },
+        {
+            title: 'This Year',
+            des: 'Hello Todalyasdf'
+        }
+    ]
 </script>
 <template>
     <div class="grid gap-8 h-screen">
@@ -12,7 +32,17 @@
 
         </header>
         <main>
-            <p>This is body</p>
+            <Tabs default-value="account" class="w-[400px">
+                <TabsList class="">
+                  <TabsTrigger v-for="item, index in list" :key="index" :value="item.title">
+                    {{item.title}}
+                  </TabsTrigger>                  
+                </TabsList>
+                <TabsContent v-for="item, index in list" :key="index" :value="item.title">
+                    <component v-if="item.component" :is="item.component" />
+                   <p v-else> {{ item.des }}</p>
+                </TabsContent>               
+              </Tabs>
         </main>
         <footer>
             <p>will have 3 card</p>
